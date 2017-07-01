95490c4ec204c022c1f8c1c1570d42c5a95e632c
mbouchett <mark@markbouchett.com>
2017-07-01 12:50:12 -0400

    now working

--- DecToHex/main.cpp
+++ DecToHex/main.cpp
@@ -9,28 +9,46 @@
 #include <iostream>  //console Input/Output
 #include <string>    //string handler
 #include "getInt.h"  // fetches an integer
+#include <c++/6/iostream>
 
 
 using namespace std; //declares the name space
 // declare function prototypes
-string decToHex(int);
+void decToHex(int, string &);
 
 int main() {
 
     getInt x;
     int z = 0;
+    string hex;
     
-    cout << "Decimal to Hexadecimal convertor\n\n";
+    cout << "Decimal to Hexadecimal convertor";
     while(z >= 0){
-        cout << "Enter an integer for conversion or -1 to quit: ";
-       x.fetchInt();
-       z = x.returnInt();
-        
-       cout << z << endl;
+        cout << "\n\nEnter an integer for conversion or -1 to quit: ";
+        hex = "";
+        x.fetchInt();
+        z = x.returnInt();
+        decToHex(z, hex);
+        if(z >=0 ){
+            cout << z << endl;
+            cout << hex;
+        }else{
+            cout << "Goodbye!";
+        }
     }
     return 0;
 }
 
-string decToHex(int){
-    
+void decToHex(int n, string& hex){
+    int dhex = 0;
+    if(!n){
+        return;
+    }else{
+        dhex = n % 16;
+        decToHex(n/16 , hex);
+    }
+    if(dhex > 9)
+        hex += 'A'+(dhex-10);
+    else
+        hex += to_string(dhex);
 }
